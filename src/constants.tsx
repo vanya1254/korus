7932001dfd91c7b57f39b2d74525df7af23ca4d3
Ivan <vanya1254@yandex.ru>
2024-05-14 18:39:34 +0300

    fix: rem noneed constants

--- src/constants.tsx
+++ src/constants.tsx
@@ -4,27 +4,6 @@ export const difficultyConvert = {
   hard: [true, true, true],
 };
 
-export const selects = {
-  cuisines: [
-    { name: "Все страны и регионы", value: "" },
-    { name: "Italian", value: "Italian" },
-  ],
-  mealTypes: [
-    { name: "Все типы", value: "" },
-    { name: "Dinner", value: "Dinner" },
-    { name: "Breakfast", value: "Breakfast" },
-    { name: "Lunch", value: "Lunch" },
-  ],
-  difficulties: [
-    { name: "Любая", value: "" },
-    { name: "Низкая", value: "Низкая" },
-    { name: "Средняя", value: "Средняя" },
-    { name: "Высокая", value: "Высокая" },
-  ],
-};
-
-export const FILTERS_NAMES = ["difficulty", "cuisine", "mealType"];
-
 export const FILTERS_LABELS = {
   cuisine: { label: "Кухня:", option: "Все страны и регионы" },
   mealType: { label: "Тип блюда:", option: "Все типы" },
@@ -32,19 +11,6 @@ export const FILTERS_LABELS = {
     label: "Сложность приготовления:",
     option: "Любая",
   },
-};
-
-export const ACTIVE_FILTERS = [
-  { name: "difficulty", value: "" },
-  { name: "cuisine", value: "" },
-  { name: "mealType", value: "" },
-];
-
-export const PARAMS = {
-  page: "",
-  cuisine: "",
-  mealType: "",
-  difficulty: "",
 };
 
 export const LIMIT = 6;
